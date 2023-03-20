dda222cf53f11e4e921bcfaa2b4b89e12db64544
C1Morgan2 <127902510+C1Morgan2@users.noreply.github.com>
2023-03-20 16:25:59 -0400

    Add files via upload

---
+++ Homework 3.13.2023.js
@@ -0,0 +1,29 @@
+//Create a variable of each data type
+//Number 
+let Number1 =  7
+let Number2 = 100.50
+//String 
+let string1 = "Today is a good day"
+let string2 = 'Yep'
+//Boolean
+let boolA = True
+let boolB = False
+//Null
+let var1 = null
+let var2 = undefined
+
+//Concatenate a string using variables to create a sentence that describes who you are  and where you live
+console.log('_____________')
+  const firstName = 'Crystal',
+  const location = 'Richmond, VA'
+  const profession = 'Loan Signing Agent'
+const yearsExperience = 4
+const aboutMe = firstName + 'is a' + profession + 'that lives in' + location + 'with' + yearsExperience + 'years experience' + '.'
+}
+//Set a variable to zero. Use at least three mathematical computations to change that value to the current year.
+Let age = 0;
+2023 = age + 2023,
+2023 = 2023 - age,
+2023 = age/age * 2023,
+
+  console.log(age)
