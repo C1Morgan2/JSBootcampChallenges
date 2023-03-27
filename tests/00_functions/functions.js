fa033196a23e6155d368d7ae0378c87ef4833f57
Crystal <Cdturner85@gmail.com>
2023-03-26 20:16:33 -0400

    Functions homework-All passed

--- tests/00_functions/functions.js
+++ tests/00_functions/functions.js
@@ -1,21 +1,45 @@
 /* eslint-disable arrow-body-style */
 /* eslint-disable consistent-return */
-function max() {
-
+function max(num1, num2, num3) {
+  if (num1 > num2 && num1 > num3) {
+    return num1
+  } else if (num2 > num1 && num2 > num3) {
+    return num2
+  } else if (num3 > num1 && num3 > num2) {
+    return num3
+  }
 }
 
-function addUnknown() {
-
+function addUnknown(n1 = 0, n2 = 0, n3 = 0, n4 = 0, n5 = 0) {
+  let total = n1 + n2 + n3 + n4 + n5
+  return total
 }
 
-function evenOrOdd() {
-
+function evenOrOdd(num1) {
+  if (num1 % 2 == 0) {
+    return "even"
+  } else {
+    return "odd"
+  }
 }
 
-function operator() {
-
+function operator(num1, num2, num3) {
+  if (num1 + num2 == num3) {
+    return "plus"
+  } else if (num1 - num2 == num3) {
+    return "minus"
+  } else if (num1 * num2 == num3) {
+    return "multiply"
+  } else if (num1 / num2 == num3) {
+    return "divide"
+  } else {
+    return null
+  }
 }
 
 module.exports = {
-  max, addUnknown, evenOrOdd, operator,
-};
+  max,
+  addUnknown,
+  evenOrOdd,
+  operator
+}
