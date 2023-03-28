0a5b22537ece6cd36c01e98bbd1e80bd146a0807
Crystal <Cdturner85@gmail.com>
2023-03-27 20:24:53 -0400

    Strings homework completed

--- tests/01_strings/strings.test.js
+++ tests/01_strings/strings.test.js
@@ -1,76 +1,74 @@
 /* eslint-disable no-undef */
-const { expect } = require('chai');
+const { expect } = require("chai")
 
-const {
-  cap, firstCharacter, doesInclude,
-} = require('./strings');
+const { cap, firstCharacter, doesInclude } = require("./strings")
 
-describe('doesInclude', () => {
-  it('is a function', () => {
-    expect(typeof doesInclude).to.equal('function');
-  });
+describe("doesInclude", () => {
+  it("is a function", () => {
+    expect(typeof doesInclude).to.equal("function")
+  })
 
-  it('returns a boolean', () => {
-    const returnedValue = doesInclude('well hello', 'g');
-    expect(typeof returnedValue).to.equal('boolean');
-  });
+  it("returns a boolean", () => {
+    const returnedValue = doesInclude("well hello", "g")
+    expect(typeof returnedValue).to.equal("boolean")
+  })
 
-  it('returns true if the string includes the character', () => {
-    const returnedValue = doesInclude('gremlin', 'm');
-    expect(returnedValue).to.equal(true);
-  });
+  it("returns true if the string includes the character", () => {
+    const returnedValue = doesInclude("gremlin", "m")
+    expect(returnedValue).to.equal(true)
+  })
 
-  it('returns false if the string does not include the character', () => {
-    const returnedValue = doesInclude('today', 'z');
-    expect(returnedValue).to.equal(false);
-  });
-});
+  it("returns false if the string does not include the character", () => {
+    const returnedValue = doesInclude("today", "z")
+    expect(returnedValue).to.equal(false)
+  })
+})
 
-describe('firstCharacter', () => {
-  it('is a function', () => {
-    expect(typeof firstCharacter).to.equal('function');
-  });
+describe("firstCharacter", () => {
+  it("is a function", () => {
+    expect(typeof firstCharacter).to.equal("function")
+  })
 
-  it('returns a boolean', () => {
-    const returnedValue = firstCharacter('hey there', 'goodbye');
-    expect(typeof returnedValue).to.equal('boolean');
-  });
+  it("returns a boolean", () => {
+    const returnedValue = firstCharacter("hey there", "goodbye")
+    expect(typeof returnedValue).to.equal("boolean")
+  })
 
-  it('returns true if both strings start with the same character', () => {
-    const returnedValue = firstCharacter('apple', 'aardvark');
-    expect(returnedValue).to.equal(true);
-  });
+  it("returns true if both strings start with the same character", () => {
+    const returnedValue = firstCharacter("apple", "aardvark")
+    expect(returnedValue).to.equal(true)
+  })
 
-  it('returns false if both strings do not start with the same character', () => {
-    const returnedValue = firstCharacter('today', 'yesterday');
-    expect(returnedValue).to.equal(false);
-  });
+  it("returns false if both strings do not start with the same character", () => {
+    const returnedValue = firstCharacter("today", "yesterday")
+    expect(returnedValue).to.equal(false)
+  })
 
-  it('*** Bonus returns true if the strings start with the same characters but are different case', () => {
-    const returnedValue = firstCharacter('bobby', 'Billy');
-    expect(returnedValue).to.equal(true);
-    expect(firstCharacter('Joe', 'tom')).to.equal(false);
-  });
-});
+  it("*** Bonus returns true if the strings start with the same characters but are different case", () => {
+    const returnedValue = firstCharacter("bobby", "Billy")
+    expect(returnedValue).to.equal(true)
+    expect(firstCharacter("Joe", "tom")).to.equal(false)
+  })
+})
 
-describe('cap', () => {
-  it('is a function', () => {
-    expect(typeof cap).to.equal('function');
-  });
+describe("cap", () => {
+  it("is a function", () => {
+    expect(typeof cap).to.equal("function")
+  })
 
-  it('returns a string', () => {
-    const returnedValue = cap('hello', 'e');
-    expect(typeof returnedValue).to.equal('string');
-  });
+  it("returns a string", () => {
+    const returnedValue = cap("hello", "e")
+    expect(typeof returnedValue).to.equal("string")
+  })
 
-  it('returns the correct string', () => {
-    const returnedValue = cap('hello', 'e');
-    expect(returnedValue).to.equal('L');
-    expect(cap('goodbye', 'b')).to.equal('Y');
-  });
+  it("returns the correct string", () => {
+    const returnedValue = cap("hello", "e")
+    expect(returnedValue).to.equal("L")
+    expect(cap("goodbye", "b")).to.equal("Y")
+  })
 
   it("returns the string 'sorry not found' if letter is not found", () => {
-    const returnedValue = cap('where are you', 'b');
-    expect(returnedValue).to.equal('sorry not found');
-  });
-});
+    const returnedValue = cap("where are you", "b")
+    expect(returnedValue).to.equal("sorry not found")
+  })
+})
