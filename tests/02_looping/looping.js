5031a268bd6d7b185f5cf1b075715e007920be1a
Crystal <Cdturner85@gmail.com>
2023-03-29 15:04:34 -0400

    Looping homework-All passed

--- tests/02_looping/looping.js
+++ tests/02_looping/looping.js
@@ -1,22 +1,47 @@
 /* eslint-disable no-param-reassign */
 /* eslint-disable no-unused-vars */
 function factorial(num) {
-
+  let total = 1
+  for (let i = 1; i <= num; i++) {
+    total *= i
+  }
+  return total
 }
 
 function stringReverse(str) {
-
+  let returnStr = ""
+  for (let i = str.length - 1; i >= 0; i--) {
+    returnStr += str[i]
+  }
+  return returnStr
 }
 
 // do not use built in slice method.
 function slicer(originalString, startIdx, endIdx) {
+  if (!startIdx) {
+    return originalString
+  }
 
+  let newStr = ""
+  for (let i = startIdx; i < endIdx; i++) {
+    newStr += originalString[i]
+  }
+  return newStr
 }
 
 function addTheEvens(num) {
-
+  let total = 0
+  for (let i = 0; i <= num; i++) {
+    if (i % 2 === 0) {
+      total += i
+    }
+  }
+  return total
 }
 
 module.exports = {
-  factorial, stringReverse, slicer, addTheEvens,
-};
+  factorial,
+  stringReverse,
+  slicer,
+  addTheEvens
+}
--- tests/02_looping/looping.test.js
+++ tests/02_looping/looping.test.js
@@ -1,96 +1,103 @@
 /* eslint-disable no-undef */
-const { expect } = require('chai');
+const { expect } = require("chai")
 
 const {
-  factorial, stringReverse, slicer, addTheEvens,
-} = require('./looping');
+  factorial,
+  stringReverse,
+  slicer,
+  addTheEvens
+} = require("./looping")
 
-describe('factorial', () => {
-  it('is a function', () => {
-    expect(typeof factorial).to.equal('function');
-  });
+describe.only("factorial", () => {
+  it("is a function", () => {
+    expect(typeof factorial).to.equal("function")
+  })
 
-  it('returns a number', () => {
-    const returnedValue = factorial(5);
-    expect(typeof returnedValue).to.equal('number');
-  });
+  it("returns a number", () => {
+    const returnedValue = factorial(5)
+    expect(typeof returnedValue).to.equal("number")
+  })
 
-  it('returns the correct value', () => {
-    const returnedValue5 = factorial(5);
-    expect(returnedValue5).to.equal(120);
-    const returnedValue7 = factorial(7);
-    expect(returnedValue7).to.equal(5040);
-  });
-});
+  it("returns the correct value", () => {
+    const returnedValue5 = factorial(5)
+    expect(returnedValue5).to.equal(120)
+    const returnedValue7 = factorial(7)
+    expect(returnedValue7).to.equal(5040)
+  })
+})
 
-describe('stringReverse', () => {
-  it('is a function', () => {
-    expect(typeof stringReverse).to.equal('function');
-  });
+describe("stringReverse", () => {
+  it("is a function", () => {
+    expect(typeof stringReverse).to.equal("function")
+  })
 
-  it('returns a string', () => {
-    const returnedValue = stringReverse('goodbye');
-    expect(typeof returnedValue).to.equal('string');
-  });
+  it("returns a string", () => {
+    const returnedValue = stringReverse("goodbye")
+    expect(typeof returnedValue).to.equal("string")
+  })
 
-  it('returns the reverse of the string goodbye', () => {
-    const returnedValue = stringReverse('goodbye');
-    expect(returnedValue).to.equal('eybdoog');
-  });
+  it("returns the reverse of the string goodbye", () => {
+    const returnedValue = stringReverse("goodbye")
+    expect(returnedValue).to.equal("eybdoog")
+  })
 
   it("returns the reverse of the string 'javascript is fun'", () => {
-    const returnedValue = stringReverse('javascript is fun');
-    expect(returnedValue).to.equal('nuf si tpircsavaj');
-  });
-});
+    const returnedValue = stringReverse("javascript is fun")
+    expect(returnedValue).to.equal("nuf si tpircsavaj")
+  })
+})
 
-describe('slicer', () => {
-  it('is a function', () => {
-    expect(typeof slicer).to.equal('function');
-  });
+describe("slicer", () => {
+  it("is a function", () => {
+    expect(typeof slicer).to.equal("function")
+  })
 
-  it('returns a string', () => {
-    const returnedValue = slicer('easy as pie');
-    expect(typeof returnedValue).to.equal('string');
-  });
+  it("returns a string", () => {
+    const returnedValue = slicer("easy as pie")
+    expect(typeof returnedValue).to.equal("string")
+  })
 
-  it('returns a new string starting at the startIdx', () => {
-    const returnedValue = slicer('twice the ice is nice', 10);
-    expect(returnedValue).to.equal('ice is nice');
-  });
+  it("returns a new string starting at the startIdx", () => {
+    const returnedValue = slicer("twice the ice is nice", 10)
+    expect(returnedValue).to.equal("ice is nice")
+  })
 
-  it('returns a new string with the correct starting and ending indices', () => {
-    const returnedValue = slicer('twice the ice is nice to slice', 6, 21);
-    expect(returnedValue).to.equal('the ice is nice');
-  });
+  it("returns a new string with the correct starting and ending indices", () => {
+    const returnedValue = slicer(
+      "twice the ice is nice to slice",
+      6,
+      21
+    )
+    expect(returnedValue).to.equal("the ice is nice")
+  })
 
-  it('returns the original string if no indices are inputted', () => {
-    const returnedValue = slicer('twice the ice is nice to slice');
-    expect(returnedValue).to.equal('twice the ice is nice to slice');
-  });
-});
+  it("returns the original string if no indices are inputted", () => {
+    const returnedValue = slicer("twice the ice is nice to slice")
+    expect(returnedValue).to.equal("twice the ice is nice to slice")
+  })
+})
 
-describe('addTheEvens', () => {
-  it('is a function', () => {
-    expect(typeof addTheEvens).to.equal('function');
-  });
+describe("addTheEvens", () => {
+  it("is a function", () => {
+    expect(typeof addTheEvens).to.equal("function")
+  })
 
-  it('returns a number', () => {
-    expect(typeof addTheEvens(10)).to.equal('number');
-  });
+  it("returns a number", () => {
+    expect(typeof addTheEvens(10)).to.equal("number")
+  })
 
-  it('returns the correct number when an even number is passed', () => {
-    const returnedValue = addTheEvens(8);
-    expect(returnedValue).to.equal(20);
-  });
+  it("returns the correct number when an even number is passed", () => {
+    const returnedValue = addTheEvens(8)
+    expect(returnedValue).to.equal(20)
+  })
 
-  it('returns the correct number when an odd number is passed', () => {
-    const returnedValue = addTheEvens(13);
-    expect(returnedValue).to.equal(42);
-  });
+  it("returns the correct number when an odd number is passed", () => {
+    const returnedValue = addTheEvens(13)
+    expect(returnedValue).to.equal(42)
+  })
 
-  it('returns the correct number when a large number is passed', () => {
-    expect(addTheEvens(111)).to.equal(3080);
-    expect(addTheEvens(220)).to.equal(12210);
-  });
-});
+  it("returns the correct number when a large number is passed", () => {
+    expect(addTheEvens(111)).to.equal(3080)
+    expect(addTheEvens(220)).to.equal(12210)
+  })
+})
